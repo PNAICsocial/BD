b6e5a4e28f4f03433b686a143a41dbc0193ac4a5
Lucas Ferreira <lucasnatacao@gmail.com>
2017-11-29 18:53:18 -0300

    Projeto Banco de Dados e POO

--- DAOUsuario.java
+++ DAOUsuario.java
@@ -44,90 +44,89 @@ public class DAOUsuario {
 		}
 		
 	}
-	}
+	
 	
 	 //busca de pessoas por seu código de identificação no banco (id)
-//	public Usuario buscarPessoa(int id_usuario) {
-//		// abrindo a conexão com o BD
-//		conexao.conectar();
-//		// busca utilizando o método de consulta do objeto ConexaoBD
-//		ResultSet resultado = conexao.executarSQL("select * from Usuario where id_usuario = \'" + id_usuario + "\'");
-//		Usuario U = new Usuario();
-//		
-//		try {
-//			resultado.next();
-//			// os métodos get devem ser escolhidos de acordo com os tipos dos atributos da entidade que está
-//			// sendo acessada
-//			id_usuario = resultado.getInt("id_usuario");
-//			String nome = resultado.getString("nome");
-//			int idade = resultado.getInt("idade");
-//			U.setid_usuario(id_usuario);
-//			U.setnome(nome);
-//			U.setidade(idade);
-//		} catch (SQLException e) {
-//			System.out.println("Erro: " + e.getMessage());
-//		} finally {
-//			// o banco deve ser desconectado, mesmo quando a exceção é lançada
-//			conexao.desconectar();
-//		}
-//		return U;
-//	}}
+	public Usuario buscarPessoa(int id_usuario) {
+		// abrindo a conexão com o BD
+		conexao.conectar();
+		// busca utilizando o método de consulta do objeto ConexaoBD
+		ResultSet resultado = conexao.executarSQL("select * from Usuario where id_usuario = \'" + id_usuario + "\'");
+		Usuario U = new Usuario();
+		
+		try {
+			resultado.next();
+			// os métodos get devem ser escolhidos de acordo com os tipos dos atributos da entidade que está
+			// sendo acessada
+			id_usuario = resultado.getInt("id_usuario");
+			String nome = resultado.getString("nome");
+			int idade = resultado.getInt("idade");
+			U.setid_usuario(id_usuario);
+			U.setnome(nome);
+			U.setidade(idade);
+		} catch (SQLException e) {
+			System.out.println("Erro: " + e.getMessage());
+		} finally {
+			// o banco deve ser desconectado, mesmo quando a exceção é lançada
+			conexao.desconectar();
+		}
+		return U;
+	}
 	
-//	public void excluirPessoa(int id_usuario) {
-//		// abrindo a conexão com o BD
-//		conexao.conectar();
-//		
-//		try {
-//			PreparedStatement stm = conexao.getConexao().prepareStatement("delete from Usuario where id_usuario = \'" + id_usuario + "\'");
-//			stm.execute();
-//		} catch (SQLException e) {
-//			System.out.println("Erro: " + e.getMessage());
-//		} finally {
-//			// o banco deve ser desconectado, mesmo quando a exceção é lançada
-//			conexao.desconectar();
-//		}
-//	}
-//
-//	public void editarPessoa(int id_usuario, String nome, int idade) {
-//		// abrindo a conexão com o BD
-//		conexao.conectar();
-//		
-//		try {
-//			PreparedStatement stm = conexao.getConexao().prepareStatement("update Usuario set nome = ?, idade = ? "
-//					+ "where cod_pessoa = \'" + id_usuario + "\'");
-//			stm.setString(1, nome);
-//			stm.setInt(2, idade);
-//			stm.execute();
-//		} catch (SQLException e) {
-//			System.out.println("Erro: " + e.getMessage());
-//		} finally {
-//			// o banco deve ser desconectado, mesmo quando a exceção é lançada
-//			conexao.desconectar();
-//		}
-//	}
-//	
-//	public ArrayList<Usuario> verTodos() {
-//		ArrayList<Usuario> pessoas = new ArrayList<>();
-//		
-//		// abrindo a conexão com o BD
-//		conexao.conectar();
-//		ResultSet resultado = conexao.executarSQL("select * from Usuario");
-//		
-//		try {
-//			// para iterar sobre os resultados de uma consulta, deve-se utilizar o método next()
-//			while (resultado.next()) {
-//				int id_usuario = resultado.getInt("id_usuario");
-//				String nome = resultado.getString("nome");
-//				int idade = resultado.getInt("idade");
-//				Usuario.add(new Usuario(id_usuario, nome, idade));
-//			}
-//		} catch (SQLException e) {
-//			System.out.println("Erro: " + e.getMessage());
-//		} finally {
-//			// o banco deve ser desconectado, mesmo quando a exceção é lançada
-//			conexao.desconectar();
-//		}
-//		return Usuario;
-//	}
-//
-//}
+	public void excluirPessoa(int id_usuario) {
+		// abrindo a conexão com o BD
+		conexao.conectar();
+		
+		try {
+			PreparedStatement stm = conexao.getConexao().prepareStatement("delete from Usuario where id_usuario = \'" + id_usuario + "\'");
+			stm.execute();
+		} catch (SQLException e) {
+			System.out.println("Erro: " + e.getMessage());
+		} finally {
+			// o banco deve ser desconectado, mesmo quando a exceção é lançada
+			conexao.desconectar();
+		}
+	}
+
+	public void editarPessoa(int id_usuario, String nome, int idade) {
+		// abrindo a conexão com o BD
+		conexao.conectar();
+		
+		try {
+			PreparedStatement stm = conexao.getConexao().prepareStatement("update Usuario set nome = ?, idade = ? "
+					+ "where cod_pessoa = \'" + id_usuario + "\'");
+			stm.setString(1, nome);
+			stm.setInt(2, idade);
+			stm.execute();
+		} catch (SQLException e) {
+			System.out.println("Erro: " + e.getMessage());
+		} finally {
+			// o banco deve ser desconectado, mesmo quando a exceção é lançada
+			conexao.desconectar();
+		}
+	}
+	
+	public ArrayList<Usuario> verTodos() {
+		ArrayList<Usuario> pessoas = new ArrayList<>();
+		
+		// abrindo a conexão com o BD
+		conexao.conectar();
+		ResultSet resultado = conexao.executarSQL("select * from Usuario");
+		
+		try {
+			// para iterar sobre os resultados de uma consulta, deve-se utilizar o método next()
+			while (resultado.next()) {
+				int id_usuario = resultado.getInt("id_usuario");
+				String nome = resultado.getString("nome");
+				int idade = resultado.getInt("idade");
+				Usuario.add(new Usuario(int id_usuario,String nome, int idade));
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro: " + e.getMessage());
+		} finally {
+		// o banco deve ser desconectado, mesmo quando a exceção é lançada
+			conexao.desconectar();		}
+		return Usuario;
+	}
+
+}

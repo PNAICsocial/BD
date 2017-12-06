902f56c19cf7419b5f07be33a60cace1d3359ee6
Lucas Ferreira <lucasnatacao@gmail.com>
2017-12-06 10:44:49 -0300

    Atualizando o Main

--- Main.java
+++ Main.java
@@ -1,25 +1,46 @@
 package controllers;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-public class Main {
+public class Main{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DAOUsuario conexao = new DAOUsuario();
+		Scanner sc = new Scanner(System.in);
 		
+		int senha = 0;
+		boolean senhaInvalida = true;
 		
+		while(senhaInvalida) {
 		
-		Usuario Pedro = new Usuario("pedro@gmail.com",1661,"Pedro",789,13,"20/04/2004","Estudante","Masculino");
-
-		Pedro.mostrarCadastrados();
+		System.out.println("Digite seu e-mail: ");
+		String email = sc.next();
 		
-	ConexaoBD conexao = new ConexaoBD();
-	conexao.conectar();
-	conexao.desconectar();
-	
-	DAOUsuario usuario = new DAOUsuario();
-	usuario.criarUsuario(Pedro);
-	
+		try{
+			System.out.println("Digite sua senha: ");
+			senha = sc.nextInt();
+			sc.next();
+			senhaInvalida = false;
+		} catch(InputMismatchException e){
+			System.out.println("Senha inválida! Digite apenas números: ");
+			
+		}
+		System.out.println("Digite seu nome: ");
+		String nome = sc.next();
+		System.out.println("Digite seu id de usuário: ");
+		int id_usuario = sc.nextInt();
+		System.out.println("Digite sua idade: ");
+		int idade = sc.nextInt();
+		System.out.println("Digite sua data de nascimento: ");
+		String data_nasc = sc.next();
+		System.out.println("Digite sua profissão: ");
+		String profissao = sc.next();
+		System.out.println("Digite seu sexo: ");
+		String sexo = sc.next();
 		
+		Usuario U = new Usuario(email, senha, nome, id_usuario, idade, data_nasc, profissao, sexo);
+		conexao.criarUsuario(U);
+		U.mostrarCadastrados();	
 	}
-
-	
+}
 }
